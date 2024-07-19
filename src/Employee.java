6d822c3ac03f6b4416f643dcf5fbf161439462c7
imbesataaquil <161199251+imbesataaquil@users.noreply.github.com>
2024-07-19 21:16:24 +0530

    Add files via upload

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+   protected int id;
+   protected int age=(int)35.5f;
+   protected String name;
+   protected boolean isPermanent;
+
+    public static void main(String[] args) {
+        System.out.println("Successfully started");
+    }
+}
